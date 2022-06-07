ead148764b40f7209354be382d0e33c602cc848b
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-06-07 15:56:38 -0300

    fix: por alguna razon no se habian subido los cambios checkeando por si `Obstaculo instanceof ObstaculoNulo` en la parte del 4x4

--- src/main/java/edu/fiuba/algo3/modelo/vehiculo/CuatroXCuatro.java
+++ src/main/java/edu/fiuba/algo3/modelo/vehiculo/CuatroXCuatro.java
@@ -1,22 +1,20 @@
 package edu.fiuba.algo3.modelo.vehiculo;
 
 import edu.fiuba.algo3.modelo.mapa.obstaculo.Obstaculo;
+import edu.fiuba.algo3.modelo.mapa.obstaculo.ObstaculoNulo;
 
 public class CuatroXCuatro extends Vehiculo {
-    private int cantidadPozosPasadosAnteriomente;
+    private int pozosPisados;
 
 	public CuatroXCuatro() {
-		this.cantidadPozosPasadosAnteriomente = 0;
+		this.pozosPisados = 0;
 	}
 
-	public void pisar(Obstaculo obstaculo) {
-		this.cantidadPozosPasadosAnteriomente++;
-
-		if (this.cantidadPozosPasadosAnteriomente < 3) {
-			return;
-		}
-
-		this.penalizaciones += obstaculo.aplicarPenalizacion(this);
-		this.cantidadPozosPasadosAnteriomente = 0;
-	}
+	public void pisar(Obstaculo pozo) {
+		if (pozo instanceof ObstaculoNulo) { return; }
+		this.pozosPisados++;
+		if (this.pozosPisados < 3) { return; }
+		this.penalizaciones += pozo.aplicarPenalizacion(this);
+		this.pozosPisados = 0;
+   }
 }
