d96be6fce30809490b4fc2e5ba8e69f8c2bf570a
RamiroRecchia <rrecchia@fi.uba.ar>
2022-06-27 13:33:22 -0300

    Agregue ayuda en pantalla inicio y boton volver a pantalla inicio

--- src/main/java/edu/fiuba/algo3/controlador/ControladorEventoVolver.java
+++ src/main/java/edu/fiuba/algo3/controlador/ControladorEventoVolver.java
@@ -1,28 +1,28 @@
 package edu.fiuba.algo3.controlador;
 
-import edu.fiuba.algo3.modelo.ModeloJuego;
 import edu.fiuba.algo3.vista.VistaAyuda;
-import edu.fiuba.algo3.vista.VistaJuego;
+import edu.fiuba.algo3.vista.VistaInicio;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class ControladorEventoAyuda implements EventHandler<ActionEvent> {
+public class ControladorEventoVolver implements EventHandler<ActionEvent> {
     private Stage stage;
     private Button boton;
 
-    public ControladorEventoAyuda(Button boton, Stage stage) {
+    public ControladorEventoVolver(Button boton, Stage stage) {
         this.boton = boton;
         this.stage = stage;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        VistaAyuda root = new VistaAyuda();
+        VistaInicio root = new VistaInicio();
         Scene scene = new Scene(root.asParent(), 1250, 750);
-        root.volver(this.stage);
+        //System.out.println("Volver");
+        root.presionarBotones(this.stage);
         stage.setScene(scene);
     }
 }
