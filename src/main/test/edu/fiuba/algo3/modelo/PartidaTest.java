129179e3d74230baf19dea6b55c12cb2095faf8c
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-06-29 15:41:58 -0300

    test: agregando los test para los getters que faltaban

---
+++ src/main/test/edu/fiuba/algo3/modelo/PartidaTest.java
@@ -0,0 +1,27 @@
+package edu.fiuba.algo3.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class PartidaTest {
+    @Test
+    public void dadoUnPuntajeSeCreaUnaPartidaConElPuntajeEsperado() {
+        Partida partida = new Partida(10);
+
+        int puntajeEsperado = 10;
+        int puntajeActual = partida.getPuntaje();
+
+        assertEquals(puntajeEsperado, puntajeActual);
+    }
+
+    @Test
+    public void alRegistrarUnaPartidaEstaContieneLaFechaDeRegistro() {
+        Partida partida = new Partida(0);
+
+        assertTrue(partida.getHoraRegistro() instanceof LocalDateTime);
+    }
+}
