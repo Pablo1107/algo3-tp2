4fd12a45bc6003f4efa0ede93e91b64acf8ba60b
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-07-04 22:51:07 -0300

    feat: mostrando listado e imagenes de resultados

--- src/main/java/edu/fiuba/algo3/vista/partidas/VistaPantallaResultados.java
+++ src/main/java/edu/fiuba/algo3/vista/partidas/VistaPantallaResultados.java
@@ -1,8 +1,5 @@
 package edu.fiuba.algo3.vista.partidas;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import edu.fiuba.algo3.controlador.ControladorBotonReiniciarPartida;
 import edu.fiuba.algo3.controlador.ControladorBotonVolver;
 import edu.fiuba.algo3.controlador.ControladorCambioDePantallas;
@@ -11,13 +8,11 @@ import edu.fiuba.algo3.vista.TituloPantalla;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class VistaPantallaResultados extends VBox {
     private static final int TAMANIO_TITULO = 36;
-    private static final String IMAGEN_GANADOR = "src/main/resources/img/ganador.png";
 
     private final ControladorCambioDePantallas controladorCambioPantallas;
 
@@ -30,8 +25,8 @@ public class VistaPantallaResultados extends VBox {
         this.getStyleClass().add("vista-pantalla-centrada");
 
         this.getChildren().add(new TituloPantalla("Fin del Juego", TAMANIO_TITULO));
-        this.getChildren().add(this.crearImagenFinalPartida());
-        this.getChildren().add(new VistaListadoResultados());
+
+        this.getChildren().add(new VistaListadoResultadosPartida());
 
         ContenedorBotones contenedorBotones = new ContenedorBotones();
         contenedorBotones.agregarBoton(this.crearBotonConControlador("Reiniciar", new ControladorBotonReiniciarPartida(this.controladorCambioPantallas)));
@@ -46,17 +41,4 @@ public class VistaPantallaResultados extends VBox {
         return boton;
     }
 
-    private ImageView crearImagenFinalPartida() {
-        ImageView imageView = new ImageView(this.getRecursoImagen(IMAGEN_GANADOR));
-        return imageView;
-    }
-
-    private Image getRecursoImagen(String archivoImagen) {
-        try {
-            FileInputStream stream = new FileInputStream(archivoImagen);
-            return new Image(stream);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
