e347026572b6e4c200c5bb662183d66d80a4bf9f
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-07-06 16:35:04 -0300

    feat: funciona el mensaje de alerta para cuando no hay jugadores

--- src/main/java/edu/fiuba/algo3/controlador/ControladorIniciarPartida.java
+++ src/main/java/edu/fiuba/algo3/controlador/ControladorIniciarPartida.java
@@ -1,22 +1,26 @@
 package edu.fiuba.algo3.controlador;
 
 import edu.fiuba.algo3.modelo.juego.Juego;
+import edu.fiuba.algo3.vista.juego.VistaPantallaRegistrarJugadores;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class ControladorIniciarPartida implements EventHandler<ActionEvent> {
     private final ControladorCambioDePantallas controladorCambioPantallas;
+    private final VistaPantallaRegistrarJugadores vistaPantallaRegistrarJugadores;
 
-    public ControladorIniciarPartida(ControladorCambioDePantallas controladorCambioPantallas) {
+    public ControladorIniciarPartida(ControladorCambioDePantallas controladorCambioPantallas, VistaPantallaRegistrarJugadores vistaPantallaRegistrarJugadores) {
         this.controladorCambioPantallas = controladorCambioPantallas;
+        this.vistaPantallaRegistrarJugadores = vistaPantallaRegistrarJugadores;
     }
 
     @Override
     public void handle(ActionEvent evento) {
         try {
             this.controladorCambioPantallas.cargarPantallaPartida(Juego.getInstancia().getPartidaActual());
+            this.vistaPantallaRegistrarJugadores.borrarAlerta();
         } catch (Exception e) {
-            System.out.println("Se intento crear una partida sin jugadores");
+            this.vistaPantallaRegistrarJugadores.setAlerta(e.getMessage());
         }
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/juego/VistaPantallaRegistrarJugadores.java
+++ src/main/java/edu/fiuba/algo3/vista/juego/VistaPantallaRegistrarJugadores.java
@@ -1,20 +1,25 @@
 package edu.fiuba.algo3.vista.juego;
 
 import edu.fiuba.algo3.App;
-import edu.fiuba.algo3.controlador.*;
+import edu.fiuba.algo3.controlador.ControladorCambioDePantallas;
+import edu.fiuba.algo3.controlador.ControladorIniciarPartida;
+import edu.fiuba.algo3.controlador.ControladorVolverAPantallaAnterior;
 import edu.fiuba.algo3.vista.ContenedorBotones;
 import edu.fiuba.algo3.vista.TituloPantalla;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class VistaPantallaRegistrarJugadores extends VBox {
     private static final int TAMANIO_TITULO = 50;
     private final ControladorCambioDePantallas controladorCambioDePantallas;
+    private final Text mensajeAlerta;
 
     public VistaPantallaRegistrarJugadores(ControladorCambioDePantallas controladorCambioDePantallas) {
         this.controladorCambioDePantallas = controladorCambioDePantallas;
+        this.mensajeAlerta = new Text();
         this.inicializarlizarVista();
     }
 
@@ -23,9 +28,10 @@ public class VistaPantallaRegistrarJugadores extends VBox {
 
         this.getChildren().add(new TituloPantalla(App.TITULO_JUEGO, TAMANIO_TITULO));
         this.getChildren().add(new VistaPromptAgregarJugadores());
+        this.getChildren().add(this.mensajeAlerta);
 
         ContenedorBotones contenedorBotones = new ContenedorBotones();
-        contenedorBotones.agregarBoton(this.crearBotonConControlador("Iniciar Partida", new ControladorIniciarPartida(this.controladorCambioDePantallas)));
+        contenedorBotones.agregarBoton(this.crearBotonConControlador("Iniciar Partida", new ControladorIniciarPartida(this.controladorCambioDePantallas, this)));
         contenedorBotones.agregarBoton(this.crearBotonConControlador("Volver", new ControladorVolverAPantallaAnterior(this.controladorCambioDePantallas, true)));
 
         this.getChildren().add(contenedorBotones);
@@ -35,5 +41,13 @@ public class VistaPantallaRegistrarJugadores extends VBox {
         Button boton = new Button(contenido);
         boton.setOnAction(controlador);
         return boton;
+    }
+
+    public void setAlerta(String contenido) {
+        this.mensajeAlerta.setText(contenido);
+    }
+
+    public void borrarAlerta() {
+        this.setAlerta("");
     }
 }
